6e520b599dc847c9021e69414d8eae7fda0fc20b
Tommaso Lunardon <tommaso.lunardon.97@gmail.com>
2025-11-22 10:10:24 -0500

    Updated prize pool

--- server.js
+++ server.js
@@ -787,8 +787,8 @@ async function startBettingPhase() {
           const { address: treasuryTokenAccount, instruction: createTreasuryTokenAccountIx } =
             await ensureTokenAccount(connection, tokenMint, treasuryPubkey, authorityKeypair.publicKey);
 
-          // Prize pool is 300k tokens in base units
-          const PRIZE_POOL = 300000;
+          // Prize pool is 30k tokens in base units
+          const PRIZE_POOL = 30000;
           const prizePoolBaseUnits = toBaseUnits(PRIZE_POOL);
 
           const tx = await program.methods
@@ -1032,7 +1032,7 @@ async function processPayouts() {
     }
 
     // Prize pool is 300k tokens, split equally among winners
-    const PRIZE_POOL = 300000;
+    const PRIZE_POOL = 30000;
     const payoutPerWinner = Math.floor(PRIZE_POOL / winnerCount);
     const payoutPerWinnerInBaseUnits = toBaseUnits(payoutPerWinner);
 
